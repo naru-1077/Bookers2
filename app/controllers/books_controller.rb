92bf856b7b4bfa9ab6716e7d255d9ab3d36f441f
EC2 Default User <ec2-user@ip-172-31-89-137.ec2.internal>
2021-02-19 14:10:02 +0000

    [Finish]4-10

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,68 @@
+class BooksController < ApplicationController
+  before_action :correct_book, only: [:edit,:update]
+  def index
+    @books = Book.all
+    @book = Book.new
+    @user = current_user
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @newbook = Book.new
+    @user = @book.user
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+        flash[:notice] = "Book was successfully created."
+        redirect_to book_path(@book)
+    else
+        @user = current_user
+        @books = Book.all
+        render :index
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+        render "edit"
+    else
+        redirect_to books_path
+    end
+  end
+
+
+  def update
+      @book = Book.find(params[:id])
+      @book.user_id = current_user.id
+      if @book.update(book_params)
+          flash[:notice] = "Book was successfully updated."
+          redirect_to book_path(@book.id)
+      else
+          render :edit
+      end
+  end
+
+  def destroy
+      @book = Book.find(params[:id])
+      if @book.destroy
+        flash[:notice] = "Book was successfully destroyed."
+        redirect_to books_path
+      end
+  end
+
+  private
+  def book_params
+      params.require(:book).permit(:title, :body)
+  end
+
+  def correct_book
+    @book = Book.find(params[:id])
+    if @book.user != current_user
+    redirect_to books_path
+    end
+  end
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '17b5403020c8a9071371ed136473d1456998b75164c4636b9c8e0b431c30b7310d3c1c6abf138fb2dfca6507a2469cebe3d5bb33ffe16e0a8cb8afa96e092494'
